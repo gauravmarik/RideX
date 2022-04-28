02b00090b33025aba7f1e435c84ba6da716e3343
marik <gaurav.marik@gmail.com>
2022-04-27 20:24:34 -0400

    carlist

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import tw from "tailwind-styled-components";
-import { carList } from "../data/carList";
+import { carList } from "../../data/carList";
 console.log("line4 RideSelector");
 
 const RideSelector = ({ pickupCoordinates, dropoffCoordinates }) => {
